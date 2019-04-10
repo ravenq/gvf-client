e1ff14074388226c4d2975c73381fd6d59d64154
fanliwen <flw_dream@126.com>
2019-04-10 14:16:40 +0800

    head & footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,13 @@
 <template>
-  <section>GVF ©2018 Created by ravenq</section>
+  <section>
+    <div>
+      Copyright © Ravenq 的技术博客2018
+      <a href="https://github.com/ravenq">
+        <a-icon type="github" style="color: #555555" />
+      </a>
+    </div>
+    <div>Power by GVF project. </div>
+  </section>
 </template>
 
 <script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const pkg = require('./package')
 module.exports = {
   mode: 'spa',
   head: {
-    title: 'ravenq 的个人主页',
+    title: 'ravenq 的技术博客',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
