a4c77c1e8c506f73399eb16b44012a4a1347fc31
ravenq <flw_dream@126.com>
2019-04-10 23:29:12 +0800

    fix ga script

--- static/google-analytics.js
+++ static/google-analytics.js
@@ -1,6 +1,4 @@
 /* eslint-disable */
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138051083-1"></script>
-
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
