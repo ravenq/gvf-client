e69b4a900b60492f76df60cc9c4094991501511c
fanliwen <flw_dream@126.com>
2019-05-25 16:38:22 +0800

    fix config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export const GITHUB = {
   OAUTH_URI: 'https://github.com/login/oauth/authorize',
-  REDIRECT_URI: 'http://localhost/login-github',
-  CLIENT_ID: '2e7a965d49ae06492a31'
+  REDIRECT_URI: 'http://www.aqcoder.com/login-github',
+  CLIENT_ID: 'bdf5348830afa9741ca0'
 }
