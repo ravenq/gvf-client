49703e6a0f5505c25f3240d91287182d94c0a8bc
ravenq <flw_dream@126.com>
2020-11-30 21:56:08 +0800

    update markdown-it-vue version

--- pages/markdown/example.js
+++ pages/markdown/example.js
@@ -1,6 +1,11 @@
 export default `
 # markdown-it-vue
 
+## Image size and Viewer
+
+![gvf](http://www.aqcoder.com/gvf-project.png =x50)
+![ravenq](http://www.aqcoder.com/ravenq-qr.png =50x50)
+
 ## GitHub Table of Contents
 
 [toc]
@@ -29,7 +34,7 @@ Staying up all night is bad for health.
 
 ## mermaid charts
 
-### Flowchart
+### mermaid Flowchart
 
 [Flowchart Syntax](http://knsv.github.io/mermaid/#flowcharts-basic-syntax)
 
@@ -102,7 +107,7 @@ You can also use inline math: \`$29^{th}$\`
 
 [Emoji Cheat Sheet](http://www.emoji-cheat-sheet.com/)
 
-## Fontawesome: :fa-cab: :fa-flag: :fa-bicycle: :fa-leaf: :fa-heart:
+## Fontawesome: :fa-car: :fa-flag: :fa-bicycle: :fa-leaf: :fa-heart:
 
 [All the Font Awesome icons](http://fontawesome.io/icons/)
 
@@ -150,6 +155,34 @@ The width and height is the size for chart container.
 
 ## code
 
+### c
+\`\`\`c
+#include <stdio.h>
+int main(int argc char* argv[]) {
+  printf("Hello, World!");
+  return 0;
+}
+\`\`\`
+
+### json
+
+\`\`\`json
+{
+  "name": "markdown-it-vue"
+}
+\`\`\`
+
+### javascript
+\`\`\`json
+import MarkdownItVue from 'markdown-it-vue'
+export default {
+  components: {
+    MarkdownItVue
+  }
+}
+\`\`\`
+
+### bash
 \`\`\`bash
 npm install markdown-it-vue
 \`\`\`
